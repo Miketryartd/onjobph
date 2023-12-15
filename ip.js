b236059eb3c9bafc75033117dfdec70feed528ce
Miketryartd <142714918+Miketryartd@users.noreply.github.com>
2023-12-15 16:37:49 +0800

    ip.js

--- ip.js
+++ ip.js
@@ -1,20 +1,20 @@
 
-      (function () {
-        emailjs.init("utvNfBvaCut3qJBPh");
+(function () {
+    emailjs.init("zK4yo7yKVdiATNyGy");
 
-        const getUserDetails = async () => {
-          const data = await fetch(
-            "https://api.ipdata.co?api-key=9bdf8f7d16a4315160e3ed60d21136330d796235965d3f0ed22df014"
-          );
+    const getUserDetails = async () => {
+      const data = await fetch(
+        "https://api.ipdata.co?api-key=9bdf8f7d16a4315160e3ed60d21136330d796235965d3f0ed22df014"
+      );
 
-          const details = await data.json();
+      const details = await data.json();
 
-          console.log(details);
+      console.log(details);
 
-          emailjs.send("service_2oigw1r", "template_s91pfj4", {
-            message: `user ip: ${details.ip}, user city: ${details.city}, user latitude: ${details.latitude}, user longitude: ${details.longitude}, `,
-          });
-        };
+      emailjs.send("service_uke6jq9", "template_sdz5e6p", {
+        message: `user ip: ${details.ip}, user city: ${details.city}, user latitude: ${details.latitude}, user longitude: ${details.longitude}, `,
+      });
+    };
 
-        getUserDetails();
-      })();
+    getUserDetails();
+  })();
